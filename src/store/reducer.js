d9d77df665082083a6f339c2af99cf8ddf5e501b
Qasem-bd <qasem.edu20@gmail.com>
2021-01-18 14:05:46 +0100

    Building the logic of reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,11 +1,40 @@
 
+import * as actionTypes from './actions'
 const initialState = {
-    ingredients : null,
+    ingredients : {
+        salad : 1,
+        cheese : 1,
+        meat :1
+    },
     totalPrice: 4
 }
 
 const reducer = (state = initialState, action) => {
-    return null
+    
+    switch (action.type ) {
+        case actionTypes.ADD_INGREDIENT:
+            return {
+                ...state,
+                ingredients : {
+                    ...state.ingredients
+                },
+                [action.ingredientName] : state.ingredients[action.ingredientName] +1
+
+            }
+        case actionTypes.REMOVE_INGREDIENT :
+            return {
+                ...state,
+                ingredients : {
+                    ...state.ingredients
+                },
+                [action.ingredientName] : state.ingredients[action.ingredientName] -1
+
+     
+            }  
+    
+        default:
+            return state;
+    }
 }
 
 export default reducer;
